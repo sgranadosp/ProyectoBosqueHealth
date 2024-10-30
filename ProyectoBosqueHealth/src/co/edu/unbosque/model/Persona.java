b616f4009a50bea1772b6e3958ab9e04b1f86703
usuario <usuario@DESKTOP-ND1MJ7T>
2024-10-30 18:01:08 -0500

    clases modelo

---
+++ ProyectoBosqueHealth/src/co/edu/unbosque/model/Persona.java
@@ -0,0 +1,72 @@
+package co.edu.unbosque.model;
+
+import java.util.Date;
+
+public class Persona {
+	
+	private String nombre;
+	private Date fechaNacimiento;
+	private String genero;
+	private int id;
+	private String correo;
+	
+	public Persona() {
+		
+	}
+
+	public Persona(String nombre, Date fechaNacimiento, String genero, int id, String correo) {
+		super();
+		this.nombre = nombre;
+		this.fechaNacimiento = fechaNacimiento;
+		this.genero = genero;
+		this.id = id;
+		this.correo = correo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	@Override
+	public String toString() {
+		return "nombre = " + nombre + ", fechaNacimiento = " + fechaNacimiento + ", genero = " + genero + ", id = "
+				+ id + ", correo = " + correo;
+	}
+	
+}
