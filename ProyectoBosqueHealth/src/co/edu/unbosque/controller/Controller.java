83efccfac470640be9bf19f25fc3ec91a94182d7
Daniel Caicedo <daniel.caicedom@outlook.com>
2024-11-02 18:42:56 -0500

    controlador

--- ProyectoBosqueHealth/src/co/edu/unbosque/controller/Controller.java
+++ ProyectoBosqueHealth/src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,37 @@
 package co.edu.unbosque.controller;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import co.edu.unbosque.model.ModelFacade;
+import co.edu.unbosque.view.ViewFacade;
+
+public class Controller implements ActionListener {
+	
+	private ModelFacade mf;
+	private ViewFacade vf;
+	
+	public Controller() {
+		mf = new ModelFacade();
+		vf = new ViewFacade();
+		asignarLectores();
+	}
+	
+	public void asignarLectores() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "":
+			
+			break;
+
+		default:
+			break;
+		}
+		
+	}
 
 }
