b8a94bf4670051574f2f8c10edab5df6547ea592
usuario <usuario@DESKTOP-ND1MJ7T>
2024-11-04 10:37:22 -0500

    asignar lectores de ventana seleccion hecho

--- ProyectoBosqueHealth/src/co/edu/unbosque/controller/Controller.java
+++ ProyectoBosqueHealth/src/co/edu/unbosque/controller/Controller.java
@@ -19,6 +19,21 @@ public class Controller implements ActionListener {
 	
 	public void asignarLectores() {
 		
+		//Menu de personas
+		//director
+		vf.getMenuPrincipal().getvSeleccion().getBtnDirector().addActionListener(this);
+		vf.getMenuPrincipal().getvSeleccion().getBtnDirector().setActionCommand("DIRECTOR");
+		
+		//especialista
+		vf.getMenuPrincipal().getvSeleccion().getBtnEspecialista().addActionListener(this);
+		vf.getMenuPrincipal().getvSeleccion().getBtnEspecialista().setActionCommand("ESPECIALISTA");
+		
+		//paciente
+		vf.getMenuPrincipal().getvSeleccion().getBtnPaciente().addActionListener(this);
+		vf.getMenuPrincipal().getvSeleccion().getBtnPaciente().setActionCommand("PACIENTE");
+		
+		
+		
 	}
 
 	@Override
