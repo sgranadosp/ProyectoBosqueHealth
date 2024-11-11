c18681d4bd403cc51300f1e67d18a0844f068544
Ricardo Javier Perez Jarro <rperez@minsalud.gov.co>
2024-11-10 22:14:42 -0500

    javadoc en aplmain

--- ProyectoBosqueHealth/src/co/edu/unbosque/controller/AplMain.java
+++ ProyectoBosqueHealth/src/co/edu/unbosque/controller/AplMain.java
@@ -4,8 +4,25 @@ import java.awt.EventQueue;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Clase principal de la aplicación, encargada de iniciar la interfaz gráfica de usuario
+ * y gestionar el lanzamiento del controlador.
+ * 
+ * Esta clase utiliza {@link EventQueue} para ejecutar el hilo de la interfaz gráfica 
+ * de manera segura y ejecuta el controlador {@link Controller} dentro de dicho hilo.
+ */
 public class AplMain {
 
+	/**
+     * Método principal que se ejecuta al iniciar la aplicación.
+     * Crea e inicializa el controlador {@link Controller} dentro del hilo de la interfaz gráfica
+     * para asegurar que la GUI se ejecute en el hilo adecuado.
+     * 
+     * En caso de que ocurra una excepción durante la creación del controlador, se muestra
+     * un mensaje de error al usuario mediante un cuadro de diálogo.
+     * 
+     * @param args Argumentos de línea de comandos. No se utilizan en esta implementación.
+     */
 	public static void main(String[] args) {
 		///	
 		//Controller c = new Controller();
