78cfbb658f65f39501bc7c3fdb56f8711eb32b99
Ricardo Javier Perez Jarro <rperez@minsalud.gov.co>
2024-11-10 20:47:29 -0500

    javadoc del paquete mail

---
+++ ProyectoBosqueHealth/src/co/edu/unbosque/util/mail/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * El paquete mail contiene la clase mail que es la encargada de poder hacer todas las funciones 
+ * de correo electronico en el programa.
+ */
+package co.edu.unbosque.util.mail;
